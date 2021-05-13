3b2a6fe884bc3153af910cbe30e88ad756570209
Uallesson <uallessons@gmail.com>
2021-05-13 10:57:41 -0300

    Desafio completo

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,19 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
+import { Container } from "./styles";
 
-import { Container } from './styles';
+import React from "react";
+import { IconBaseProps } from "react-icons/lib";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface inputProps {
+  name: string;
+  placeholder?: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: inputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -31,7 +34,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
